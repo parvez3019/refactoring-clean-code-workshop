0dc99d27c00309794923fe07a2b96793cebc5ac0
parvez3019 <pha3019@gmail.com>
2022-05-05 12:27:39 +0530

    Day 1 | Session 2 End of Day 1 Code

--- src/main/java/com/olx/movierental/Customer.java
+++ src/main/java/com/olx/movierental/Customer.java
@@ -4,40 +4,89 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Customer {
-  private String name;
-  private List<Rental> rentals = new ArrayList<>();
+    private String name;
+    private List<Rental> rentals = new ArrayList<>();
 
-  public Customer(String name) {
-    this.name = name;
-  }
-
-  public void addRental(Rental arg) {
-    rentals.add(arg);
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public String statement() {
-    double totalAmount = 0;
-    int totalFrequentRenterPoints = 0;
-    String statement = "Rental Record for " + getName() + "\n";
-    for (Rental rental : rentals) {
-      totalFrequentRenterPoints += rental.frequentRenterPoints();
-
-      //show figures for this rental
-      statement += "\t" + rental.getMovie().getTitle() + "\t" +
-          String.valueOf(rental.amount()) + "\n";
-      totalAmount += rental.amount();
+    public Customer(String name) {
+        this.name = name;
     }
 
-    //add footer lines statement
-    statement += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-    statement += "You earned " + String.valueOf(totalFrequentRenterPoints)
-        + " frequent renter points";
-    return statement;
-  }
+    public void addRental(Rental arg) {
+        rentals.add(arg);
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public String statement() {
+        return header() + body() + footer();
+    }
+
+    private String header() {
+        return "Rental Record for " + getName() + "\n";
+    }
+
+    private String footer() {
+        String statement = "Amount owed is " + String.valueOf(totalAmount()) + "\n";
+        statement += "You earned " + String.valueOf(totalFrequentRenterPoints())
+                + " frequent renter points";
+        return statement;
+    }
+
+    private double totalAmount() {
+        double totalAmount = 0;
+        for (Rental rental : rentals) {
+
+            totalAmount += rental.amount();
+        }
+        return totalAmount;
+    }
+
+    private String body() {
+        String statement = "";
+        for (Rental rental : rentals) {
+
+            //show figures for this rental
+            statement += "\t" + rental.getMovie().getTitle() + "\t" +
+                    String.valueOf(rental.amount()) + "\n";
+        }
+        return statement;
+    }
+
+    private int totalFrequentRenterPoints() {
+        int totalFrequentRenterPoints = 0;
+        for (Rental rental : rentals) {
+            totalFrequentRenterPoints += rental.frequentRenterPoints();
+
+        }
+        return totalFrequentRenterPoints;
+    }
+
+    public String htmlStatement() {
+        return htmlHeader() + htmlBody() + htmlFooter();
+    }
+
+    private String htmlFooter() {
+        String statement = "Amount owed is <b>" + String.valueOf(totalAmount()) + "</b><br/>";
+        statement += "You earned <b>" + String.valueOf(totalFrequentRenterPoints())
+                + "</b> frequent renter points";
+        return statement;
+    }
+
+    private String htmlBody() {
+        String statement = "";
+        for (Rental rental : rentals) {
+
+            //show figures for this rental
+            statement += rental.getMovie().getTitle() + " " +
+                    String.valueOf(rental.amount()) + "<br/>";
+        }
+        return statement;
+    }
+
+    private String htmlHeader() {
+        return "<h1>Rental Record for <i>" + getName() + "</i></h1><br/>";
+    }
 }
 
--- src/test/java/com/olx/movierental/CustomerTest.java
+++ src/test/java/com/olx/movierental/CustomerTest.java
@@ -14,7 +14,7 @@ public class CustomerTest {
 
         String actualStatement = customer.statement();
 
-        String statement ="Rental Record for Harsh\n" +
+        String statement = "Rental Record for Harsh\n" +
                 "\tDie Hard 4.0\t3.5\n" +
                 "\tIrish Man\t3.0\n" +
                 "\tToy Story\t3.0\n" +
@@ -22,5 +22,26 @@ public class CustomerTest {
                 "You earned 3 frequent renter points";
         Assert.assertEquals(statement, actualStatement);
     }
+
+    @Test
+    public void shouldGenerateHTMLStatement() {
+        Customer customer = new Customer("Harsh");
+
+        customer.addRental(new Rental(new Movie("Die Hard 4.0", 0), 3));
+        customer.addRental(new Rental(new Movie("Irish Man", 1), 1));
+        customer.addRental(new Rental(new Movie("Toy Story", 2), 4));
+
+        String actualStatement = customer.htmlStatement();
+
+        String statement = "<h1>Rental Record for <i>Harsh</i></h1><br/>" +
+                "Die Hard 4.0 3.5<br/>" +
+                "Irish Man 3.0<br/>" +
+                "Toy Story 3.0<br/>" +
+                "Amount owed is <b>9.5</b><br/>" +
+                "You earned <b>3</b> frequent renter points";
+
+        Assert.assertEquals(statement, actualStatement);
+    }
+
 
 }
