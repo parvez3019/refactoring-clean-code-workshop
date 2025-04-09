d0072c84d155af8c30dbfec5f42fac839d4e30e5
AnuragKumar2704 <anurag.kumar@olx.in>
2025-04-10 00:16:58 +0530

    write test case for currencyConvertor and ExchangeRates

--- src/test/java/com/olx/testingexercises/exercise3/CurrencyConverterTest.java
+++ src/test/java/com/olx/testingexercises/exercise3/CurrencyConverterTest.java
@@ -11,16 +11,16 @@ class CurrencyConverterTest {
 
     @Test
     void convert() {
-        // Arrange
+
         ExchangeRateService rateService = mock(ExchangeRateService.class);
         when(rateService.getRate("USD", "INR")).thenReturn(82.0);
 
         CurrencyConverter converter = new CurrencyConverter(rateService);
 
-        // Act
+
         double result = converter.convert("USD", "INR", 10);
 
-        // Assert
+
         assertEquals(820.0, result, 0.001);
 
     }
