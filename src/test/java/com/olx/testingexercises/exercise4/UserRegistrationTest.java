dbf3ca627596c6afeb42650bfa483ff666e93955
shreyaOLX <shreya.shukla@olx.in>
2025-04-10 00:08:29 +0530

    test UserRegistration test

--- src/test/java/com/olx/testingexercises/exercise4/UserRegistrationTest.java
+++ src/test/java/com/olx/testingexercises/exercise4/UserRegistrationTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 class UserRegistrationTest {
     private EmailService mockemailService ;
@@ -12,11 +12,25 @@ class UserRegistrationTest {
     @BeforeEach
     public void setUp() {
         mockemailService = mock(EmailService.class);
+        userRegistration = new UserRegistration(mockemailService);
     }
     @Test
-    void registerUser() {
+    void registerUser_shouldSendEmailOnce() {
+        String email = "shreya@example.com";
+        userRegistration.register(email);
 
+        verify(mockemailService, times(1)).sendEmail(email, "Welcome " + email);
     }
+
+    @Test
+    void registerUserTwice_shouldSendEmailTwice() {
+        String email = "shreya@example.com";
+        userRegistration.register(email);
+        userRegistration.register(email);
+
+        verify(mockemailService, times(2)).sendEmail(email, "Welcome " + email);
+    }
+
 
 
 }
