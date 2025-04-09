c53d1df92ac095f28f9cb45272d5b50da06f7182
AnuragKumar2704 <anurag.kumar@olx.in>
2025-04-09 22:32:38 +0530

    fork the repo

---
+++ src/test/java/com/olx/testingexercises/exercise1/TemperatureConverterTest.java
@@ -0,0 +1,16 @@
+package com.olx.testingexercises.exercise1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TemperatureConverterTest {
+
+    @Test
+    void celsiusToFahrenheit() {
+    }
+
+    @Test
+    void fahrenheitToCelsius() {
+    }
+}
