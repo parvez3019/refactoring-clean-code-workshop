20c4ef7c28d8e123c7ca6e390812fa21ce0562a7
Naina Prashar <naina.prashar@olx.in>
2025-04-10 01:24:47 +0530

    Test Cases For User Registration

---
+++ src/test/java/com/olx/testingexercises/exercise4/UserRegistrationTest.java
@@ -0,0 +1,33 @@
+package com.olx.testingexercises.exercise4;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class UserRegistrationTest {
+    private EmailService mockemailService ;
+    private UserRegistration userRegistration;
+    @BeforeEach
+    public void setUp() {
+        mockemailService = mock(EmailService.class);
+        userRegistration = new UserRegistration(mockemailService);
+    }
+    @Test
+    void registerUser_shouldSendEmailOnce() {
+        String email = "shreya@example.com";
+        userRegistration.register(email);
+
+        verify(mockemailService, times(1)).sendEmail(email, "Welcome " + email);
+    }
+
+    @Test
+    void registerUserTwice_shouldSendEmailTwice() {
+        String email = "shreya@example.com";
+        userRegistration.register(email);
+        userRegistration.register(email);
+
+        verify(mockemailService, times(2)).sendEmail(email, "Welcome " + email);
+    }
+}
