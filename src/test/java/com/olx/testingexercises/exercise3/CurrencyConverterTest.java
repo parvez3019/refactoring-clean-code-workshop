d0650eae56638aac2c6a243787ecdf028a2de7e9
AkashSingh-Coder <akash.singh1@olx.in>
2025-04-09 21:33:10 +0530

    demo commit

---
+++ src/test/java/com/olx/testingexercises/exercise3/CurrencyConverterTest.java
@@ -0,0 +1,12 @@
+package com.olx.testingexercises.exercise3;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CurrencyConverterTest {
+
+    @Test
+    void convert() {
+    }
+}
---
+++ src/test/java/com/olx/testingexercises/exercise3/ExchangeRateServiceTest.java
@@ -0,0 +1,11 @@
+package com.olx.testingexercises.exercise3;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ExchangeRateServiceTest {
+
+    @Test
+    void getRate() {
+    }
+}
