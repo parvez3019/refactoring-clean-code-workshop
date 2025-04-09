1f5644ae91d6fb1872b705e7f5dcc3cf4e8db580
Ashutosh Kumar <ashutosh.kumar@olx.in>
2025-04-09 14:35:34 +0530

    written ut for TemperatureConverter

---
+++ src/test/java/com/olx/testingexercises/exercise1/TemperatureConverterTest.java
@@ -0,0 +1,39 @@
+package com.olx.testingexercises.exercise1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TemperatureConverterTest {
+
+    @Test
+    public void shouldSuccessfullyConvertToFahrenheit() {
+        //Arrange
+        double tempInCelsius = 37;
+        TemperatureConverter converter = new TemperatureConverter();
+
+        //Act
+        double convertedToFahrenheit = converter.celsiusToFahrenheit(tempInCelsius);
+
+        //Assert
+        assertEquals(98.6, convertedToFahrenheit, 0.001);
+
+    }
+
+    @Test
+    public void shouldSuccessfullyConvertToCelsius() {
+        //Arrange
+        double tempInFahrenheit = 37;
+        TemperatureConverter converter = new TemperatureConverter();
+
+        //Act
+        double convertedToCelsius = converter.fahrenheitToCelsius(tempInFahrenheit);
+
+        //Assert
+        assertEquals(2.777, convertedToCelsius, 0.001);
+    }
+
+
+
+
+}
