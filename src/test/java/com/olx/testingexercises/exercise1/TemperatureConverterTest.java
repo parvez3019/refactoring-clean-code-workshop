5d4306537cca2b6771354bd7bb1c7392e79e0e07
Nirbhay <nirbhay.gupta@olx.in>
2025-04-09 20:49:12 +0530

    creating temperature converter tester

---
+++ src/test/java/com/olx/testingexercises/exercise1/TemperatureConverterTest.java
@@ -0,0 +1,36 @@
+package com.olx.testingexercises.exercise1;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TemperatureConverterTest {
+    TemperatureConverter temperatureConverter;
+    @BeforeEach
+    void setUp(){
+        temperatureConverter = new TemperatureConverter();
+    }
+
+    @Test
+    void celsiusToFahrenheit() {
+        double result = temperatureConverter.celsiusToFahrenheit(30);
+        assertEquals(86.00, result, 0.01);
+    }
+
+    @Test
+    void fahrenheitToCelsius() {
+        double result = temperatureConverter.fahrenheitToCelsius(30);
+        assertEquals(-1.11, result, 0.01);
+    }
+
+    @AfterEach
+    void tearDown(){
+        temperatureConverter = null;
+    }
+
+
+
+
+}
