b6e297203ea81a6e7eb454971f8f22e5b3f05e22
Yash Sharma <yash.sharma@olx.in>
2025-04-09 22:48:43 +0530

    Add Unit Test for exercise 1-TemperatureConverter

--- src/test/java/com/olx/testingexercises/exercise1/TemperatureConverterTest.java
+++ src/test/java/com/olx/testingexercises/exercise1/TemperatureConverterTest.java
@@ -1,35 +1,41 @@
 package com.olx.testingexercises.exercise1;
 
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TemperatureConverterTest {
 
-    @Test
-    public void testCelsiusToFahrenheit() {
+    private TemperatureConverter temperatureConverter;
 
-        // Arrange
-        TemperatureConverter temperatureConverter = new TemperatureConverter();
-
-        // Act
-        double fahrenheit = temperatureConverter.celsiusToFahrenheit(30);
-
-        // Assert
-        assertEquals(86.0 , fahrenheit, 0);
-
-
+    @BeforeEach
+    void setUp() {
+        temperatureConverter = new TemperatureConverter();
     }
 
     @Test
-    public void testFahrenheitToCelsius() {
-        // Arrange
-        TemperatureConverter temperatureConverter = new TemperatureConverter();
-
-        // Act
-        double celsius = temperatureConverter.fahrenheitToCelsius(68);
-
-        // Assert
-        assertEquals(20.0 , celsius,0);
+    public void testCelsiusToFahrenheit_PositiveAndZeroInput() {
+        assertEquals(32.0, temperatureConverter.celsiusToFahrenheit(0), 0);
+        assertEquals(212.0, temperatureConverter.celsiusToFahrenheit(100), 0);
+        assertEquals(98.6, temperatureConverter.celsiusToFahrenheit(37), 0);
     }
+
+    @Test
+    public void testCelsiusToFahrenheit_NegativeInput() {
+        assertEquals(-40.0, temperatureConverter.celsiusToFahrenheit(-40), 0);
+    }
+
+    @Test
+    public void testFahrenheitToCelsius_PositiveAndZeroInput() {
+        assertEquals(0.0, temperatureConverter.fahrenheitToCelsius(32), 0);
+        assertEquals(-17.77777777777778, temperatureConverter.fahrenheitToCelsius(0), 0);
+        assertEquals(100.0, temperatureConverter.fahrenheitToCelsius(212), 0);
+    }
+
+    @Test
+    public void testFahrenheitToCelsius_NegativeInput() {
+        assertEquals(-40.0, temperatureConverter.fahrenheitToCelsius(-40), 0);
+    }
+
 }
