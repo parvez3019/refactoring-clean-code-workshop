152fdb1c8cc2460e3eefb31c0e256527b5121e61
anujgarg3366 <anuj.garg@olx.in>
2025-04-13 17:22:25 +0530

    updated UserRegistrationTest

--- src/test/java/com/olx/testingexercises/exercise4/UserRegistrationTest.java
+++ src/test/java/com/olx/testingexercises/exercise4/UserRegistrationTest.java
@@ -1,21 +1,21 @@
 package com.olx.testingexercises.exercise4;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.engine.extension.ExtensionRegistry;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 class UserRegistrationTest {
 
     @Test
-    void register() {
+    void shouldSendEmailTwiceWhenRegisteringTwoUsers() {
         EmailService emailService = mock(EmailService.class);
         UserRegistration userRegistration = new UserRegistration(emailService);
+        String firstUser = "user1@example.com";
+        String secondUser = "user2@example.com";
 
-        userRegistration.register(anyString());
-        userRegistration.register(anyString());
+        userRegistration.register(firstUser);
+        userRegistration.register(secondUser);
 
-        verify(emailService,times(2)).sendEmail(anyString(), anyString());
+        verify(emailService, times(2)).sendEmail(anyString(), anyString());
     }
 }
