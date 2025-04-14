7626ee6898fa3dd890701090ed3da9ba8aa6e09f
Ashutosh Kumar <ashutosh.kumar@olx.in>
2025-04-15 03:45:26 +0530

    exercise 2 ut

--- src/test/java/com/olx/testingexercises/exercise2/SessionManagerTest.java
+++ src/test/java/com/olx/testingexercises/exercise2/SessionManagerTest.java
@@ -1,21 +1,29 @@
 package com.olx.testingexercises.exercise2;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SessionManagerTest {
 
-    @Test
-    void startSession() {
-
+    SessionManager sessionManager;
+    @BeforeEach
+    public void beginSession() {
+        sessionManager = new SessionManager();
+        assertFalse(sessionManager.isSessionActive(), "Session should be active after starting");
+        sessionManager.startSession();
     }
 
     @Test
-    void endSession() {
+    public void testIsSessionActive() {
+        assertTrue(sessionManager.isSessionActive(), "Session should be active");
     }
 
-    @Test
-    void isSessionActive() {
+    @AfterEach
+    public void endSession() {
+        sessionManager.endSession();
+        assertFalse(sessionManager.isSessionActive(), "Session should be not active");
     }
 }
